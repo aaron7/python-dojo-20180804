a5bcb04f98e9d651287ad3b69f2ad08c10f680b5
Timothy McCurrach <tim.mccurrach@gmail.com>
2018-10-04 20:01:22 +0100

    Implement get_options

--- simple/simple.py
+++ simple/simple.py
@@ -5,6 +5,15 @@ def main():
 
 def get_options():
     # Gathers options from stdin and returns list
+    options = []
+    print("Enter your amazing Dojo Idea (Enter 'FINISH' when done):")
+    while True:
+        new_option = input()
+        if new_option == "FINISH":
+            return options
+        if new_option:
+            options.append(new_option)
+
 
 def perform_voting(options):
     # Iterates over options and accepts votes
