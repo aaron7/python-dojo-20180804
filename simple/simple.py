3560b4cb8d2fe91f63bb5c4c67882c3d9535fb26
Aaron Kirkbride <aaron@aaronkirkbride.com>
2018-10-04 19:50:28 +0100

    Add boilerplate for simple

---
+++ simple/simple.py
@@ -0,0 +1,14 @@
+
+def main():
+    options = get_options()
+    perform_voting(options)
+
+def get_options():
+    # Gathers options from stdin and returns list
+
+def perform_voting(options):
+    # Iterates over options and accepts votes
+
+
+if __name__ == "__main__":
+    main()
